37f72542a5f5f55a9e4232fc4a902deeb05a28da
Shilpa106 <shilpadevi.bluethink@gmail.com>
2021-08-12 19:59:30 +0530

    f

---
+++ django_email_send/emailceleryapp/views.py
@@ -0,0 +1,12 @@
+from django.core.mail import send_mail
+from django.shortcuts import render
+from django.http import HttpResponse
+from .tasks import sleepy, send_email_task
+
+
+# Create your views here.
+def index(request):
+    send_email_task.delay()
+    # sleepy.delay(10)
+    return HttpResponse('<h1>EMAIL HAS BEEN SENT WITH CELERY!</h1>')
+
